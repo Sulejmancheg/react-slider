11d052931b7b9341b238243fb77813a9a3a47dfd
Sulejmancheg <52045282+Sulejmancheg@users.noreply.github.com>
2021-07-01 17:16:58 +0300

    Slider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Arrow(props) {
+    return (
+        <button
+            className="Arrow"
+            onClick={props.onClick}
+        >
+            &lt;
+        </button>
+    );
+}
+
+class Figure extends React.Component {
+    renderArrow(i) {
+        return (
+            <Arrow
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <figure className="Slider">
+                <img id="image" src={this.props.image} alt=""/>
+                <div className="Arrows">
+                    {this.renderArrow(0)}
+                    {this.renderArrow(1)}
+                </div>
+            </figure>
+        );
+    }
+}
+
+
+class Slider extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            position: 1,
+        }
+    }
+
+    handleClick(i) {
+
+        let position = this.state.position;
+        if (i === 0) {
+            position--;
+            if (position < 1) {
+                position = 5;
+            }
+        } else {
+            position++;
+            if (position > 5) {
+                position = 1;
+            }
+        }
+        this.setState({
+            position: position,
+        });
+
+    }
+
+    render(){
+        const image = './img/test_image_' + this.state.position + '.jpg';
+
+        return (
+            <Figure
+                image={image}
+                onClick={(i) => this.handleClick(i)}
+            />
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Slider />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
